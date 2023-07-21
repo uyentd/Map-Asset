8f828314261484b733ad159848e99e5c0c1eabb5
uyentd <55289179+uyentd@users.noreply.github.com>
2023-07-21 22:25:43 +0700

    update cs

---
+++ Assets/MainScript/CameraControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform PBRCharacter;
+    public Transform trans;
+    //private Vector3 rotationMove;
+   // private Vector3 offet = new Vector3(0,4f,-10f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        ////Xác định hướng di chuyển của character
+        //Vector3 moveDirection = PBRCharacter.forward;
+        //moveDirection.y = 0f;// trục Y = 0, ko di chuyển
+        //if (rotationMove != Vector3.zero)
+        //{
+        //    Quaternion tagetRotation = Quaternion.LookRotation(rotationMove);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, tagetRotation, Time.deltaTime * 10f);
+        //}
+        trans.position = PBRCharacter.transform.position;
+       // trans.position = this.PBRCharacter.transform.position + this.offet;
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+        ////Điều chỉnh góc quay
+        //rotationMove.x += horizontalInput;
+        //rotationMove.y += verticalInput;
+        ////Áp dụng cho camera
+        //transform.eulerAngles = rotationMove;
+    }
+
+}
+                    
--- Assets/MainScript/MoveCharacter.cs
+++ Assets/MainScript/MoveCharacter.cs
@@ -1,18 +1,39 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveCharacter : MonoBehaviour
 {
+    public float speed = 2f ;
+    public Transform trans ;
+    public Animator animator ;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.LogError(" speed: " + speed);
         
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         
+        float x = Input.GetAxis("Horizontal");// GetAxis : lấy giá trị từ 0 -> 1 or -1 ( có 0.1,...)
+        float z = Input.GetAxis("Vertical");
+        Vector3 pos_new = new Vector3(x, 0, z);
+        float length = pos_new.magnitude; //tạo 1 độ dài
+        if ( length > 0 )
+        {
+            trans.forward = pos_new;
+            trans.position = trans.position + (pos_new * speed * Time.deltaTime);
+            animator.SetFloat("speed", 1);
+        }
+        else
+        {
+            animator.SetFloat("speed", 0);
+        }
+        //trans.position = new Vector3( trans.position.x + x*speed, trans.position.y, trans.position.z + z* speed );
     }
 }
